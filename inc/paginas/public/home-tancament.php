ba449cef1f270b0fa1e8de1911b4b67d2269f370
Luis Elía <luis@elpuig.xeill.net>
2026-09-09 12:35:28 +0200

    Cambio de texto en bloque especialmente melifluo

--- inc/paginas/public/home-tancament.php
+++ inc/paginas/public/home-tancament.php
@@ -1,11 +1,11 @@
 <section class="col-12 mb-5">
     <div class="home-tancament">
         <p class="home-tancament__eyebrow">Projectes</p>
-        <h2 class="home-tancament__title">Projectes que deixen rastre</h2>
+        <h2 class="home-tancament__title">Sobre aquesta web</h2>
         <p class="home-tancament__text">
-            Aquest espai recull els projectes realitzats per l’alumnat de l’Institut Puig Castellar
-            i els posa a disposició de la comunitat educativa. Serveix per consultar projectes ja
-            realitzats, entendre com s’han resolt i trobar noves idees per als que vindran.
+            Aquest espai recull els projectes realitzats per l’alumnat dels cicles formatius d’Informàtica de l’Institut Puig Castellar
+            i els posa a disposició de la comunitat educativa. Serveix per consultar projectes ja realitzats
+            i veure com s’han plantejat i desenvolupat.
         </p>
     </div>
 </section>
